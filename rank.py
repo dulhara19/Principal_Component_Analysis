997fa8cd51a189328511ff81d6ad80abbcc0608b
dulhara <wkldulhara@gmail.com>
2025-05-01 23:38:19 +0530

    new

--- rank.py
+++ rank.py
@@ -37,7 +37,7 @@ matrix1 = [
 
 print(rank_of_matrix(matrix1))
 
-#----------------------Principal component analysis using rank ------------------------------------
+#----------------------Principal component analysis using rank -end-----------------------------------
 import numpy as np
 import pandas as pd
 import seaborn as sns
